deb5608d0784c7b0a869587b99246b7fd9da47d3
semih turan <semituran@gmail.com>
2024-08-01 12:18:38 +0300

    add comments sections

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
-(function(){
+(function () {
     'use strict';
+  
+    AOS.init({
+      startEvent: 'load',
+      offset: 20,
+      once: true,
+    });
+  
     var camera = new Swiper('#camera .swiper', {
-        speed: 600,
-        spaceBetween: 12,
-        navigation: {
-            enabled: true,
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }
+      speed: 600,
+      spaceBetween: 12,
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
     });
-
+  
+    var glightbox = GLightbox({
+      selector: '.glightbox',
+    })
+  
     var photos = GLightbox({
-        selector: '#photos .photos'
+      selector: '#photos .photo',
     });
-})();
+  
+    var comment = new Swiper('#comments .swiper', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+      }
+    });
+  
+  })();
